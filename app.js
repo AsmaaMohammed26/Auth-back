9c22a6b8bf3fa0b1c4a6f859aaecb765eac5d748
AsmaaMohammed26 <AsmaamGomaa26@gmail.com>
2025-07-05 16:40:10 +0300

    add vercel json

--- app.js
+++ app.js
@@ -1,46 +1,35 @@
-const express = require ('express');
-const helmet = require ('helmet');
-const cors = require ('cors');
-const dotenv = require ('dotenv');
-const hpp = require ('hpp');
-const dbConnect = require ('./Config/dataBase');
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const hpp = require('hpp');
+const dbConnect = require('./Config/dataBase');
 
-const errorMW = require ('./Middlewares/errorMW');
-const userRoute = require ('./Routes/userRoute');
+const errorMW = require('./Middlewares/errorMW');
+const userRoute = require('./Routes/userRoute');
+const authRoute = require('./Routes/authRoute');
 
-dotenv.config ({path: './config.env'});
+dotenv.config({ path: './config.env' });
 
-const app = express ();
-const PORT = process.env.PORT || 5025;
+const app = express();
 
 // Connect to database
-dbConnect ();
+dbConnect();
 
 // Middlewares
-app.use (hpp ());
-app.use (helmet ());
-app.use (cors ());
-app.use (express.json ());
-app.use (express.urlencoded ({extended: true}));
+app.use(hpp());
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use ('/api/v1/users', userRoute);
-const authRoute = require ('./Routes/authRoute');
-app.use ('/api/v1/auth', authRoute);
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/auth', authRoute);
 
-app.use (errorMW);
-// Start server
-const server = app.listen (PORT, () => {
-  console.log (
-    `Server is running on port *********************************${PORT}`
-  );
-});
+// Error Middleware
+app.use(errorMW);
 
-// Handle unhandled promise rejections 
-process.on ('unhandledRejection', error => {
-  console.error (`Unhandled Rejection: ${error.name} - ${error.message}`);
-  server.close (() => {
-    console.log ('Server closed due to unhandled rejection');
-    process.exit (1);
-  });
-});
+// ✅ Vercel will use this exported app
+module.exports = app;
+
